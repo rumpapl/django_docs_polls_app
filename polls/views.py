4ec9ec5da379ba8b4a5f9def17f11a633648d5b5
rumpapl <rumpakucse14@gmail.com>
2021-10-07 19:49:51 +0600

    worked on voting feature, [after successfully dealing with POST data, it redirected to results page]

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
 # External imports
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Internal imports
-from .models import Question
+from .models import Choice, Question
 
 
 # Create your views here.
@@ -29,4 +29,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
