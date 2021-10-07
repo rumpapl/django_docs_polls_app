ccd8aecb5fe0a6c81042f31b6e64861762f1bbda
rumpapl <rumpakucse14@gmail.com>
2021-10-07 17:48:19 +0600

    worked on creating new views, rendered template from view

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,33 @@
+# External imports
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+# Internal imports
+from .models import Question
+
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, You'ar at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
-def contact(request):
-    return HttpResponse("Hello, You'ar at the polls Contact page.")
+def detail(request, question_id):
+    return HttpResponse("Hello, You'ar looking at question %s." % question_id)
 
 
-def test(request):
-    return HttpResponse("Hello, You'ar at the polls Test page.")
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
