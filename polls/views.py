db4a79b1b14c76fc6933f92d62092cb5bb870a76
rumpapl <rumpakucse14@gmail.com>
2021-10-07 18:00:36 +0600

    render html-template using shortcut:render()

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 # External imports
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 
 
 # Internal imports
@@ -13,11 +13,10 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
